f481ec72ae111cefd5406842df421db974276236
Manuel Alejandro Rey Cruz <e-marey@neoris.com>
2022-07-05 11:46:47 -0500

    Fianl

--- src/main/java/org/PDO/servicio/PersonaServicio.java
+++ src/main/java/org/PDO/servicio/PersonaServicio.java
@@ -1,5 +1,6 @@
 package org.PDO.servicio;
 
+import java.util.Calendar;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -11,6 +12,8 @@ import org.PDO.repositorio.PersonaRepositorio;
 @ApplicationScoped
 public class PersonaServicio implements PersonaServicioInt{
 
+    int year = Calendar.getInstance().get(Calendar.YEAR);
+
     @Inject
     PersonaRepositorio personaRepositorio;
 
@@ -19,14 +22,13 @@ public class PersonaServicio implements PersonaServicioInt{
         return personaRepositorio.findById(id);
     }
 
-    
     @Transactional 
     public Persona addPersona(PersonaDto personaDto){
-        if(Integer.parseInt(personaDto.fechaNacimiento)<=1959 && Integer.parseInt(personaDto.fechaNacimiento)>=2004 ){
+        if(Integer.parseInt(personaDto.fechaNacimiento)<=(year - 63) || Integer.parseInt(personaDto.fechaNacimiento)>=(year - 18) ){
             throw new WebApplicationException("Persona no registrada", 404);
         }
         Persona persona = new Persona(personaDto.nombre,personaDto.primerApellido,personaDto.segundoApellido,personaDto.fechaNacimiento,personaDto.domicilio);
-        persona.edad = 2022 - Integer.parseInt(personaDto.fechaNacimiento);
+        persona.edad = year - Integer.parseInt(personaDto.fechaNacimiento);
         persona.fechaNacimiento = persona.fechaNacimiento + "/01/01";
         persona.persist();   
         return persona;
@@ -38,13 +40,13 @@ public class PersonaServicio implements PersonaServicioInt{
         if(persona == null){
             throw new WebApplicationException("Persona no registrada", 404);
         }else{
-            if(Integer.parseInt(personaDto.fechaNacimiento)>=1959 && Integer.parseInt(personaDto.fechaNacimiento)<=2004 ){
+            if(Integer.parseInt(personaDto.fechaNacimiento)>=(year - 63) && Integer.parseInt(personaDto.fechaNacimiento)<=( year - 18) ){
                 persona.nombre = personaDto.nombre;
                 persona.primerApellido = personaDto.primerApellido;
                 persona.segundoApellido = personaDto.segundoApellido;
                 persona.fechaNacimiento = personaDto.fechaNacimiento;
                 persona.domicilio = personaDto.domicilio;
-                persona.edad = 2022 - Integer.parseInt(personaDto.fechaNacimiento);
+                persona.edad = year - Integer.parseInt(personaDto.fechaNacimiento);
                 persona.fechaNacimiento = persona.fechaNacimiento + "/01/01";
                 persona.persist();
                 return persona;
